9ea979de99d8d7526155187d8ac16477e3fa5fe8
jeremymees <jeremymees123@gmail.com>
2024-03-01 15:16:52 +0100

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss'
   ],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: '~/tailwind.config.ts',
+    injectPosition: 0,
+    viewer: false,
+    exposeConfig: true
+  },
   devtools: { enabled: true }
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+export const content = [
+  './app.vue',
+  './components/*.{vue,js}'
+]
+
+export const theme = {
+  extend: {
+    fontFamily: {
+      mukta: ['Mukta', 'sans-serif'],
+      madi: ['MsMadi', 'sans-serif']
+    },
+    colors: {
+      white: '#FEFDF7',
+      black: '#0F0E0A',
+      primary: '#315546',
+      secondary: '#C35200'
+    }
+  }
+}
+
+export const plugins = []
