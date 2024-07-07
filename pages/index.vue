700a05a2420b0876e08169bc30525aa4854b95a6
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-07-07 10:40:03 +0200

    feat: tweaked subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ onBeforeUnmount(() => store.unsubscribe())
       </h1>
       <div class="flex flex-col justify-center gap-[10px] font-light text-[18px] leading-[29px] max-w-5xl text-center mx-auto">
         <p>
-          In de kalender krijg je een overzicht van onze events en openingstijden. Klik op één van de vakken om te boeken.
+          In de kalender krijg je een overzicht van onze events en openingstijden. Klik op één van de vakken om de exacte uren te zien en te boeken.
         </p>
         <p>
           Tijdens het open spelcafé staat onze deur open voor iedereen. Op de donkergroene momenten kan je Nooi reserveren voor je eigen activiteit. Dat is gratis, tenzij je Nooi voor jezelf wilt.
