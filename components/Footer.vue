3ff2c2f36d1c27808288f45dcb0b24bc23b9fece
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-09-29 14:26:19 +0200

    feat: added whatsapp to the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,44 +11,55 @@
           href="https://www.google.be/maps/place/Nooi/@50.9845556,5.0496567,17z/data=!3m1!4b1!4m5!3m4!1s0x47c13f6fa13d98f1:0x204404adae3e0327!8m2!3d50.9845311!4d5.0519057"
           target="_blank"
           aria-label="Locatie"
-          class="bg-black p-2 rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
+          class="bg-black w-8 h-8 flex flex-col justify-center items-center rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
         >
           <Icon
             name="carbon:location-filled"
-            class="text-white text-2xl"
+            class="text-white text-lg"
           />
         </a>
         <a
           href="https://www.instagram.com/nooidiest/"
           target="_blank"
           aria-label="Instagram"
-          class="bg-black p-2 rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
+          class="bg-black w-8 h-8 flex flex-col justify-center items-center rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
         >
           <Icon
             name="radix-icons:instagram-logo"
-            class="text-white text-2xl"
+            class="text-white text-lg"
           />
         </a>
         <a
           href="https://www.facebook.com/nooidiest"
           target="_blank"
           aria-label="Facebook"
-          class="bg-black p-2 rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
+          class="bg-black w-8 h-8 flex flex-col justify-center items-center rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
         >
           <Icon
             name="ic:baseline-facebook"
-            class="text-white text-2xl"
+            class="text-white text-lg"
           />
         </a>
         <a
           href="https://www.tiktok.com/@nooidiest"
           target="_blank"
           aria-label="Tiktok"
-          class="bg-black p-2 rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
+          class="bg-black w-8 h-8 flex flex-col justify-center items-center rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
         >
           <Icon
             name="ic:baseline-tiktok"
-            class="text-white text-2xl"
+            class="text-white text-lg"
+          />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=32496672361&text&type=phone_number&app_absent=0"
+          target="_blank"
+          aria-label="Tiktok"
+          class="bg-black w-8 h-8 flex flex-col justify-center items-center rounded hover:opacity-80 transition-opacity duration-300 ease-in-out"
+        >
+          <Icon
+            name="ic:baseline-whatsapp"
+            class="text-white text-lg"
           />
         </a>
       </div>
