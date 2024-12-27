aac54ebe272f85ef1bfac14bed07a9a56e8a0acd
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-12-27 09:16:07 +0100

    feat: loading indicator

--- app.vue
+++ app.vue
@@ -4,6 +4,10 @@ useSeo()
 
 <template>
   <div>
+    <NuxtLoadingIndicator
+      color="linear-gradient(to right,#315546 70%,#53A688 100%)"
+      error-color="#C35200"
+    />
     <NuxtPage />
     <Toast />
   </div>
