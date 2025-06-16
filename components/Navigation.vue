e821427dc8ed8f8fa051899e179978da4299eb0c
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-06-16 19:14:41 +0200

    feat: bigger nav links for mobile

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,11 +2,17 @@
   <nav class="pt-[10px]">
     <div class="flex flex-col sm:flex-row sm:items-center max-w-[750px] mx-auto py-[10px]">
       <div class="h-[100px] flex flex-col justify-center flex-1">
-        <div class="flex flex-wrap justify-center gap-x-16 font-bold body-normal">
-          <NuxtLink to="/">
+        <div class="flex flex-wrap justify-center gap-x-16">
+          <NuxtLink
+            to="/"
+            class="nav-link"
+          >
             kalender
           </NuxtLink>
-          <NuxtLink to="https://nooi.be/aanbod/">
+          <NuxtLink
+            to="https://nooi.be/aanbod/"
+            class="nav-link"
+          >
             aanbod
           </NuxtLink>
         </div>
@@ -18,11 +24,17 @@
         NOOI
       </NuxtLink>
       <div class="h-[100px] flex flex-col justify-center flex-1">
-        <div class="flex flex-wrap justify-center gap-x-16 font-bold body-normal">
-          <NuxtLink to="https://nooi.be/locatie/">
+        <div class="flex flex-wrap justify-center gap-x-16">
+          <NuxtLink
+            to="https://nooi.be/locatie/"
+            class="nav-link"
+          >
             locatie
           </NuxtLink>
-          <NuxtLink to="https://nooi.be/info/">
+          <NuxtLink
+            to="https://nooi.be/info/"
+            class="nav-link"
+          >
             info
           </NuxtLink>
         </div>
