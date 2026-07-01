0b148ef431ca55b513e59933fc671cf932b4068d
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-07-01 08:35:41 +0200

    feat: home content change

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,10 +13,10 @@ onBeforeUnmount(() => store.unsubscribe())
       </h1>
       <div class="flex flex-col justify-center gap-[10px] font-light text-[18px] leading-[29px] max-w-5xl text-center mx-auto">
         <p>
-          In de kalender krijg je een overzicht van onze events en openingstijden. Klik op één van de vakken om de exacte uren te zien en te boeken.
+          In de kalender krijg je een overzicht van onze events en openingstijden. Klik op een vak of een event om te reserveren.
         </p>
         <p>
-          Tijdens het open spelcafé staat onze deur open voor iedereen. Op de donkergroene momenten kan je Nooi reserveren voor je eigen activiteit. Dat is gratis, tenzij je Nooi voor jezelf wilt.
+          Tijdens het open spelcafé (lichtgroen) staat onze deur open voor iedereen. Op de donkergroene momenten kan je Nooi reserveren voor je eigen activiteit. Dat is gratis, tenzij je Nooi voor jezelf wilt.
         </p>
       </div>
     </div>
